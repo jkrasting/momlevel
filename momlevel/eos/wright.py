b2c713074805f7cf5ece2bbd3309a9cc5f609e55
John Krasting <John.Krasting@noaa.gov>
2021-07-02 14:36:33 -0400

    Modifications to Wright EoS function
    - Added support for multi-dimensional arrays
    - Currently assumes input arrays are in axis order
      of (depth,lat,lon) and does a transpose
    - Depth must be the right-most axis for broadcasting
      rules in NumPy

--- momlevel/eos/wright.py
+++ momlevel/eos/wright.py
@@ -1,30 +1,53 @@
-def wright_eos(T,S,p):
-  """
+import numpy as np
 
- **********************************************************************
-   The subroutines in this file implement the equation of state for   *
-   sea water using the formulae given by  Wright, 1997, J. Atmos.     *
-   Ocean. Tech., 14, 735-740.  Coded by R. Hallberg, 7/00.            *
- ***********************************************************************
-    Converted to Python from F90 by M Harrison 10/11.
 
- Calculate seawater equation of state, given T[degC],S[PSU],p[Pa]
- Returns density [kg m-3]
+def wright(T, S, p):
+    """Calculate in-situ density based on the Wright equation of state.
 
- ***********************************************************************
+    Reference:
+      Wright, 1997, J. Atmos. Ocean. Tech., 14, 735-740.
 
- """
+    Parameters
+    ----------
+    T : numpy.ndarray
+        Sea water potential temperature in deg C
+    S : numpy.ndarray
+        Sea water practical salinity in PSU
+    p : numpy.ndarray
+        Sea water pressure in Pa
 
-  a0 = 7.057924e-4; a1 = 3.480336e-7; a2 = -1.112733e-7;
-  b0 = 5.790749e8;  b1 = 3.516535e6;  b2 = -4.002714e4;
-  b3 = 2.084372e2;  b4 = 5.944068e5;  b5 = -9.643486e3;
-  c0 = 1.704853e5;  c1 = 7.904722e2;  c2 = -7.984422;
-  c3 = 5.140652e-2; c4 = -2.302158e2; c5 = -3.079464;
+    Returns
+    -------
+    numpy.ndarray
+        Sea water in-situ density in kg m-3
+    """
 
-  al0 = a0 + a1*T +a2*S
-  p0  = b0 + b4*S + T * (b1 + T*(b2 + b3*T) + b5*S)
-  lam = c0 +c4*S + T * (c1 + T*(c2 + c3*T) + c5*S)
-  I_denom = 1.0 / (lam + al0*(p+p0))
-  rho = (p + p0) * I_denom
+    # p needs to be the right-most axis
+    T = np.swapaxes(T, -3, -1)
+    S = np.swapaxes(S, -3, -1)
 
-  return rho
+    a0 = 7.057924e-4
+    a1 = 3.480336e-7
+    a2 = -1.112733e-7
+    b0 = 5.790749e8
+    b1 = 3.516535e6
+    b2 = -4.002714e4
+    b3 = 2.084372e2
+    b4 = 5.944068e5
+    b5 = -9.643486e3
+    c0 = 1.704853e5
+    c1 = 7.904722e2
+    c2 = -7.984422
+    c3 = 5.140652e-2
+    c4 = -2.302158e2
+    c5 = -3.079464
+
+    al0 = a0 + a1 * T + a2 * S
+    p0 = b0 + b4 * S + T * (b1 + T * (b2 + b3 * T) + b5 * S)
+    lam = c0 + c4 * S + T * (c1 + T * (c2 + c3 * T) + c5 * S)
+    I_denom = 1.0 / (lam + al0 * (p + p0))
+    result = (p + p0) * I_denom
+
+    result = np.swapaxes(result, -3, -1)
+
+    return result
