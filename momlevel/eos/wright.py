772d7f5e86e9d7b08a348e85055a2cc76fe6f8ee
John Krasting <John.Krasting@noaa.gov>
2022-01-20 07:36:24 -0500

    Broke out constants for Wright EOS

--- momlevel/eos/wright.py
+++ momlevel/eos/wright.py
@@ -1,5 +1,23 @@
 """ Wright Equation of State """
 
+# constants
+A0 = 7.057924e-4
+A1 = 3.480336e-7
+A2 = -1.112733e-7
+B0 = 5.790749e8
+B1 = 3.516535e6
+B2 = -4.002714e4
+B3 = 2.084372e2
+B4 = 5.944068e5
+B5 = -9.643486e3
+C0 = 1.704853e5
+C1 = 7.904722e2
+C2 = -7.984422
+C3 = 5.140652e-2
+C4 = -2.302158e2
+C5 = -3.079464
+
+
 def wright(T, S, p):
     """Calculate in-situ density based on the Wright equation of state.
 
@@ -21,25 +39,9 @@ def wright(T, S, p):
         Sea water in-situ density in kg m-3
     """
 
-    a0 = 7.057924e-4
-    a1 = 3.480336e-7
-    a2 = -1.112733e-7
-    b0 = 5.790749e8
-    b1 = 3.516535e6
-    b2 = -4.002714e4
-    b3 = 2.084372e2
-    b4 = 5.944068e5
-    b5 = -9.643486e3
-    c0 = 1.704853e5
-    c1 = 7.904722e2
-    c2 = -7.984422
-    c3 = 5.140652e-2
-    c4 = -2.302158e2
-    c5 = -3.079464
-
-    al0 = a0 + a1 * T + a2 * S
-    p0 = b0 + b4 * S + T * (b1 + T * (b2 + b3 * T) + b5 * S)
-    lam = c0 + c4 * S + T * (c1 + T * (c2 + c3 * T) + c5 * S)
+    al0 = A0 + A1 * T + A2 * S
+    p0 = B0 + B4 * S + T * (B1 + T * (B2 + B3 * T) + B5 * S)
+    lam = C0 + C4 * S + T * (C1 + T * (C2 + C3 * T) + C5 * S)
     I_denom = 1.0 / (lam + al0 * (p + p0))
     result = (p + p0) * I_denom
 
