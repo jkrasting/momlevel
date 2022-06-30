c60bcba7a0e6a1b2ec6cfaa3d0c812fb72f8cef8
John Krasting <John.Krasting@noaa.gov>
2022-06-30 07:12:14 -0400

    Added tidegauge module

---
+++ momlevel/tidegauge.py
@@ -0,0 +1,10 @@
+""" tidegauge.py - tide gague tools for momlevel """
+
+import xarray as xr
+
+__all__ = ["extract_tidegauge"]
+
+
+def extract_tidegauge(arr):
+    result = None
+    return result
