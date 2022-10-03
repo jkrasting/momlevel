43b2bc68e0645e0f23e87555b8de28311f4a343f
John Krasting <John.Krasting@noaa.gov>
2022-10-03 16:55:34 -0400

    Readthedocs environment updates
    * Updated readthedocs install method
    * Pin importlib_metadata to < v5
      - Causes xarray netcdf load error
      - See https://github.com/python/importlib_metadata/issues/406
    * Update .readthedocs.yaml
    * Update requirements.txt
    * Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+package_path = os.path.abspath('../..')
+os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
 # -- Project information -----------------------------------------------------
 
