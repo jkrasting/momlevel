8bada10224891b9e9f19fdb9a378ff36eaf4ad6b
John Krasting <John.Krasting@noaa.gov>
2022-10-04 18:32:26 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 
-package_path = os.path.abspath('../..')
+package_path = os.path.abspath('../../src')
 os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
 # -- Project information -----------------------------------------------------
