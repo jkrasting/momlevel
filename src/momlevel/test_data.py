80e12d01d1f5041a40df24e47ba01cbcb44de22a
John Krasting <John.Krasting@noaa.gov>
2023-01-21 15:10:51 -0500

    Separate funcs to h and z points

--- src/momlevel/test_data.py
+++ src/momlevel/test_data.py
@@ -15,6 +15,128 @@ __all__ = [
 ]
 
 
+def _add_h_point_fields(dset=None):
+
+    dset = xr.Dataset if dset is None else dset
+
+    dset["xh"] = xr.DataArray(
+        [1.0, 2.0, 3.0, 4.0, 5.0],
+        dims="xh",
+        attrs={
+            "long_name": "h point nominal longitude",
+            "units": "degrees_east",
+            "axis": "X",
+            "cartesian_axis": "X",
+        },
+    )
+
+    dset["yh"] = xr.DataArray(
+        [1.0, 2.0, 3.0, 4.0, 5.0],
+        dims="yh",
+        attrs={
+            "long_name": "h point nominal latitude",
+            "units": "degrees_north",
+            "axis": "Y",
+            "cartesian_axis": "Y",
+        },
+    )
+
+    # geolon / geolat as cell centers
+    lon = np.arange(0.0, 361.0, 72.0)
+    lat = np.arange(-90.0, 91.0, 36.0)
+
+    lon = [(lon[x] + lon[x + 1]) / 2.0 for x in range(0, len(lon) - 1)]
+    lat = [(lat[x] + lat[x + 1]) / 2.0 for x in range(0, len(lat) - 1)]
+    geolon, geolat = np.meshgrid(lon, lat)
+
+    dset["geolon"] = xr.DataArray(
+        geolon,
+        dims=("yh", "xh"),
+        attrs={
+            "long_name": "Longitude of tracer (T) points",
+            "units": "degrees_east",
+            "cell_methods": "time: point",
+        },
+    )
+
+    dset["geolat"] = xr.DataArray(
+        geolat,
+        dims=("yh", "xh"),
+        attrs={
+            "long_name": "Latitude of tracer (T) points",
+            "units": "degrees_north",
+            "cell_methods": "time: point",
+        },
+    )
+
+    areacello = xr.DataArray(
+        np.random.normal(100.0, 10.0, (5, 5)),
+        dims=({"yh": dset.yh, "xh": dset.xh}),
+    )
+    areacello = areacello / areacello.sum()
+    dset["areacello"] = areacello * 3.6111092e14
+    dset["areacello"].attrs = {
+        "long_name": "Ocean Grid-Cell Area",
+        "units": "m2",
+        "cell_methods": "area:sum yh:sum xh:sum time: point",
+        "standard_name": "cell_area",
+    }
+    return dset
+
+
+def _add_z_level_fields(dset=None):
+    dset = xr.Dataset() if dset is None else dset
+
+    dset["z_i"] = xr.DataArray(
+        np.array([0.0, 5.0, 15.0, 185.0, 1815.0, 6185.0]),
+        dims=("z_i"),
+        attrs={
+            "long_name": "Depth at interface",
+            "units": "meters",
+            "axis": "Z",
+            "positive": "down",
+        },
+    )
+
+    dset["z_l"] = xr.DataArray(
+        np.array([2.5, 10.0, 100.0, 1000.0, 4000.0]),
+        dims=("z_l"),
+        attrs={
+            "long_name": "Depth at cell center",
+            "units": "meters",
+            "axis": "Z",
+            "positive": "down",
+            "edges": "z_i",
+        },
+    )
+
+    deptho = np.array(
+        [
+            np.random.uniform(0.0, 5.0, 5),
+            np.random.uniform(0.0, 15.0, 5),
+            np.random.uniform(0.0, 185.0, 5),
+            np.random.uniform(0.0, 1815.0, 5),
+            np.random.uniform(0.0, 6185.0, 5),
+        ]
+    )
+
+    dset["deptho"] = xr.DataArray(
+        deptho,
+        dims=({"yh": dset.yh, "xh": dset.xh}),
+        attrs={
+            "long_name": "Sea Floor Depth",
+            "units": "m",
+            "cell_methods": "area:mean yh:mean xh:mean time: point",
+            "cell_measures": "area: areacello",
+            "standard_name": "sea_floor_depth_below_geoid",
+        },
+    )
+
+    # TO-DO: move zl, zi, and deptho in here
+
+    return dset
+
+
 def generate_daily_timeaxis(start_year=1979, nyears=2, calendar="noleap"):
     """Function to generate a daily time axis for testing
 
@@ -89,78 +211,8 @@ def generate_test_data(start_year=1981, nyears=0, calendar="noleap", seed=123):
 
     ntimes = len(dset["time"])
 
-    dset["z_i"] = xr.DataArray(
-        np.array([0.0, 5.0, 15.0, 185.0, 1815.0, 6185.0]),
-        dims=("z_i"),
-        attrs={
-            "long_name": "Depth at interface",
-            "units": "meters",
-            "axis": "Z",
-            "positive": "down",
-        },
-    )
-
-    dset["z_l"] = xr.DataArray(
-        np.array([2.5, 10.0, 100.0, 1000.0, 4000.0]),
-        dims=("z_l"),
-        attrs={
-            "long_name": "Depth at cell center",
-            "units": "meters",
-            "axis": "Z",
-            "positive": "down",
-            "edges": "z_i",
-        },
-    )
-
-    dset["xh"] = xr.DataArray(
-        [1.0, 2.0, 3.0, 4.0, 5.0],
-        dims="xh",
-        attrs={
-            "long_name": "h point nominal longitude",
-            "units": "degrees_east",
-            "axis": "X",
-            "cartesian_axis": "X",
-        },
-    )
-
-    dset["yh"] = xr.DataArray(
-        [1.0, 2.0, 3.0, 4.0, 5.0],
-        dims="yh",
-        attrs={
-            "long_name": "h point nominal latitude",
-            "units": "degrees_north",
-            "axis": "Y",
-            "cartesian_axis": "Y",
-        },
-    )
-
-    # geolon / geolat as cell centers
-    lon = np.arange(0.0, 361.0, 72.0)
-    lat = np.arange(-90.0, 91.0, 36.0)
-
-    lon = [(lon[x] + lon[x + 1]) / 2.0 for x in range(0, len(lon) - 1)]
-    lat = [(lat[x] + lat[x + 1]) / 2.0 for x in range(0, len(lat) - 1)]
-    geolon, geolat = np.meshgrid(lon, lat)
-
-    dset["geolon"] = xr.DataArray(
-        geolon,
-        dims=("yh", "xh"),
-        attrs={
-            "long_name": "Longitude of tracer (T) points",
-            "units": "degrees_east",
-            "cell_methods": "time: point",
-        },
-    )
-
-    dset["geolat"] = xr.DataArray(
-        geolat,
-        dims=("yh", "xh"),
-        attrs={
-            "long_name": "Latitude of tracer (T) points",
-            "units": "degrees_north",
-            "cell_methods": "time: point",
-        },
-    )
+    dset = _add_h_point_fields(dset)
+    dset = _add_z_level_fields(dset)
 
     dset["thetao"] = xr.DataArray(
         np.random.default_rng(seed).normal(15.0, 5.0, (ntimes, 5, 5, 5)),
@@ -200,41 +252,6 @@ def generate_test_data(start_year=1981, nyears=0, calendar="noleap", seed=123):
             "time_avg_info": "average_T1,average_T2,average_DT",
         },
     )
-
-    deptho = np.array(
-        [
-            np.random.default_rng(seed).uniform(0.0, 5.0, 5),
-            np.random.default_rng(seed).uniform(0.0, 15.0, 5),
-            np.random.default_rng(seed).uniform(0.0, 185.0, 5),
-            np.random.default_rng(seed).uniform(0.0, 1815.0, 5),
-            np.random.default_rng(seed).uniform(0.0, 6185.0, 5),
-        ]
-    )
-
-    dset["deptho"] = xr.DataArray(
-        deptho,
-        dims=({"yh": dset.yh, "xh": dset.xh}),
-        attrs={
-            "long_name": "Sea Floor Depth",
-            "units": "m",
-            "cell_methods": "area:mean yh:mean xh:mean time: point",
-            "cell_measures": "area: areacello",
-            "standard_name": "sea_floor_depth_below_geoid",
-        },
-    )
-
-    areacello = xr.DataArray(
-        np.random.default_rng(seed).normal(100.0, 10.0, (5, 5)),
-        dims=({"yh": dset.yh, "xh": dset.xh}),
-    )
-    areacello = areacello / areacello.sum()
-    dset["areacello"] = areacello * 3.6111092e14
-    dset["areacello"].attrs = {
-        "long_name": "Ocean Grid-Cell Area",
-        "units": "m2",
-        "cell_methods": "area:sum yh:sum xh:sum time: point",
-        "standard_name": "cell_area",
-    }
 
     return dset
 
