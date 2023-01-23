b3128684445501ed97703b12d197fdb833d471a3
John Krasting <John.Krasting@noaa.gov>
2023-01-23 11:40:54 -0500

    Refactored test_data as a stand-alone module

--- src/momlevel/test_data/__init__.py
+++ src/momlevel/test_data/__init__.py
@@ -1,167 +1,16 @@
-""" test_data.py - module for generating test data """
+""" test_data - module for generating test data """
 
-import datetime as dt
-
-import cftime
 import numpy as np
 import xarray as xr
 
+from .time import generate_time_stub
+from .tripolar import xy_fields, zlevel_fields
+
 __all__ = [
-    "generate_daily_timeaxis",
     "generate_test_data",
     "generate_test_data_dz",
-    "generate_test_data_time",
     "generate_test_data_uv",
 ]
-
-
-def _add_h_point_fields(dset=None):
-
-    dset = xr.Dataset if dset is None else dset
-
-    dset["xh"] = xr.DataArray(
-        [1.0, 2.0, 3.0, 4.0, 5.0],
-        dims="xh",
-        attrs={
-            "long_name": "h point nominal longitude",
-            "units": "degrees_east",
-            "axis": "X",
-            "cartesian_axis": "X",
-        },
-    )
-
-    dset["yh"] = xr.DataArray(
-        [1.0, 2.0, 3.0, 4.0, 5.0],
-        dims="yh",
-        attrs={
-            "long_name": "h point nominal latitude",
-            "units": "degrees_north",
-            "axis": "Y",
-            "cartesian_axis": "Y",
-        },
-    )
-
-    # geolon / geolat as cell centers
-    lon = np.arange(0.0, 361.0, 72.0)
-    lat = np.arange(-90.0, 91.0, 36.0)
-
-    lon = [(lon[x] + lon[x + 1]) / 2.0 for x in range(0, len(lon) - 1)]
-    lat = [(lat[x] + lat[x + 1]) / 2.0 for x in range(0, len(lat) - 1)]
-    geolon, geolat = np.meshgrid(lon, lat)
-
-    dset["geolon"] = xr.DataArray(
-        geolon,
-        dims=("yh", "xh"),
-        attrs={
-            "long_name": "Longitude of tracer (T) points",
-            "units": "degrees_east",
-            "cell_methods": "time: point",
-        },
-    )
-
-    dset["geolat"] = xr.DataArray(
-        geolat,
-        dims=("yh", "xh"),
-        attrs={
-            "long_name": "Latitude of tracer (T) points",
-            "units": "degrees_north",
-            "cell_methods": "time: point",
-        },
-    )
-
-    areacello = xr.DataArray(
-        np.random.normal(100.0, 10.0, (5, 5)),
-        dims=({"yh": dset.yh, "xh": dset.xh}),
-    )
-    areacello = areacello / areacello.sum()
-    dset["areacello"] = areacello * 3.6111092e14
-    dset["areacello"].attrs = {
-        "long_name": "Ocean Grid-Cell Area",
-        "units": "m2",
-        "cell_methods": "area:sum yh:sum xh:sum time: point",
-        "standard_name": "cell_area",
-    }
-    return dset
-
-
-def _add_z_level_fields(dset=None):
-    dset = xr.Dataset() if dset is None else dset
-
-    dset["z_i"] = xr.DataArray(
-        np.array([0.0, 5.0, 15.0, 185.0, 1815.0, 6185.0]),
-        dims=("z_i"),
-        attrs={
-            "long_name": "Depth at interface",
-            "units": "meters",
-            "axis": "Z",
-            "positive": "down",
-        },
-    )
-
-    dset["z_l"] = xr.DataArray(
-        np.array([2.5, 10.0, 100.0, 1000.0, 4000.0]),
-        dims=("z_l"),
-        attrs={
-            "long_name": "Depth at cell center",
-            "units": "meters",
-            "axis": "Z",
-            "positive": "down",
-            "edges": "z_i",
-        },
-    )
-
-    deptho = np.array(
-        [
-            np.random.uniform(0.0, 5.0, 5),
-            np.random.uniform(0.0, 15.0, 5),
-            np.random.uniform(0.0, 185.0, 5),
-            np.random.uniform(0.0, 1815.0, 5),
-            np.random.uniform(0.0, 6185.0, 5),
-        ]
-    )
-
-    dset["deptho"] = xr.DataArray(
-        deptho,
-        dims=({"yh": dset.yh, "xh": dset.xh}),
-        attrs={
-            "long_name": "Sea Floor Depth",
-            "units": "m",
-            "cell_methods": "area:mean yh:mean xh:mean time: point",
-            "cell_measures": "area: areacello",
-            "standard_name": "sea_floor_depth_below_geoid",
-        },
-    )
-
-    # TO-DO: move zl, zi, and deptho in here
-
-    return dset
-
-
-def generate_daily_timeaxis(start_year=1979, nyears=2, calendar="noleap"):
-    """Function to generate a daily time axis for testing
-
-    Parameters
-    ----------
-    start_year : int, optional
-        Starting year for timeseries, by default 1979
-    nyears : int, optional
-        Number of years for test data, by default 2
-    calendar : str, optional
-        Valid `cftime` calendar, by default "noleap"
-
-    Returns
-    -------
-    List[cftime._cftime.Datetime]
-        List of cftime datetime objects for specified calendar
-    """
-
-    init = cftime.datetime(start_year, 1, 1, calendar=calendar)
-    endtime = cftime.datetime(start_year + nyears, 1, 1, calendar=calendar)
-
-    days = [init + dt.timedelta(days=x) for x in range(0, 366 * nyears)]
-    days = [x for x in days if x < endtime]
-
-    return days
 
 
 def generate_test_data(start_year=1981, nyears=0, calendar="noleap", seed=123):
@@ -211,8 +60,8 @@ def generate_test_data(start_year=1981, nyears=0, calendar="noleap", seed=123):
 
     ntimes = len(dset["time"])
 
-    dset = _add_h_point_fields(dset)
-    dset = _add_z_level_fields(dset)
+    dset = xy_fields(dset)
+    dset = zlevel_fields(dset)
 
     dset["thetao"] = xr.DataArray(
         np.random.default_rng(seed).normal(15.0, 5.0, (ntimes, 5, 5, 5)),
@@ -376,40 +225,78 @@ def generate_test_data_uv(start_year=1981, nyears=0, calendar="noleap", seed=123
         )
     else:
         dset = xr.Dataset()
-        dset["time"] = xr.DataArray([1.0, 2.0, 3.0, 4.0, 5.0], dims=("time"))
+        dset["time"] = xr.DataArray(
+            [1.0, 2.0, 3.0, 4.0, 5.0],
+            dims=("time"),
+            attrs={
+                "long_name": "time",
+                "cartesian_axis": "T",
+                "calendar_type": calendar,
+                "bounds": "time_bnds",
+            },
+        )
 
     ntimes = len(dset["time"])
 
-    dset["z_i"] = xr.DataArray(
-        np.array([0.0, 5.0, 15.0, 185.0, 1815.0, 6185.0]), dims=("z_i")
-    )
-    dset["z_l"] = xr.DataArray(
-        np.array([2.5, 10.0, 100.0, 1000.0, 4000.0]), dims=("z_l")
-    )
-    dset["xh"] = xr.DataArray([1.0, 2.0, 3.0, 4.0, 5.0], dims="xh")
+    dset = xy_fields(dset)
+    dset = zlevel_fields(dset)
+
     dset["xq"] = xr.DataArray([1.5, 2.5, 3.5, 4.5, 5.5], dims="xq")
-    dset["yh"] = xr.DataArray([1.0, 2.0, 3.0, 4.0, 5.0], dims="yh")
     dset["yq"] = xr.DataArray([1.5, 2.5, 3.5, 4.5, 5.5], dims="yq")
 
     dset["uo"] = xr.DataArray(
         np.random.default_rng(seed).normal(0.0061, 0.08, (ntimes, 5, 5, 5)),
         dims=({"time": dset.time, "z_l": dset.z_l, "yh": dset.yh, "xq": dset.xq}),
+        attrs={
+            "long_name": "Sea Water X Velocity",
+            "units": "m s-1",
+            "standard_name": "sea_water_x_velocity",
+            "interp_method": "none",
+            "cell_methods": "z_l:mean yh:mean xq:point time: mean",
+            "time_avg_info": "average_T1,average_T2,average_DT",
+        },
     )
     dset["vo"] = xr.DataArray(
         np.random.default_rng(seed).normal(0.00077, 0.04, (ntimes, 5, 5, 5)),
         dims=({"time": dset.time, "z_l": dset.z_l, "yq": dset.yq, "xh": dset.xh}),
+        attrs={
+            "long_name": "Sea Water Y Velocity",
+            "units": "m s-1",
+            "standard_name": "sea_water_y_velocity",
+            "interp_method": "none",
+            "cell_methods": "z_l:mean yq:point xh:mean time: mean",
+            "time_avg_info": "average_T1,average_T2,average_DT",
+        },
     )
     dset["dxCu"] = xr.DataArray(
         np.ones((5, 5)),
         dims=({"yh": dset.yh, "xq": dset.xq}),
+        attrs={
+            "long_name": "Delta(x) at u points (meter)",
+            "units": "m",
+            "cell_methods": "time: point",
+            "interp_method": "none",
+        },
     )
     dset["dyCv"] = xr.DataArray(
         np.ones((5, 5)),
         dims=({"yq": dset.yq, "xh": dset.xh}),
+        attrs={
+            "long_name": "Delta(y) at v points (meter)",
+            "units": "m",
+            "cell_methods": "time: point",
+            "interp_method": "none",
+        },
     )
     dset["Coriolis"] = xr.DataArray(
         np.random.default_rng(seed).normal(1.21e-5, 0.00011, (5, 5)),
         dims=({"yq": dset.yq, "xq": dset.xq}),
+        attrs={
+            "long_name": "Coriolis parameter at corner (Bu) points",
+            "units": "s-1",
+            "cell_methods": "time: point",
+            "interp_method": "none",
+        },
     )
     areacello_bu = xr.DataArray(
         np.random.default_rng(seed).normal(100.0, 10.0, (5, 5)),
@@ -418,85 +305,11 @@ def generate_test_data_uv(start_year=1981, nyears=0, calendar="noleap", seed=123
     areacello_bu = areacello_bu / areacello_bu.sum()
     dset["areacello_bu"] = areacello_bu * 3.6111092e14
 
+    dset["areacello_bu"].attrs = {
+        "long_name": "Ocean Grid-Cell Area",
+        "units": "m2",
+        "cell_methods": "area:sum yq:sum xq:sum time: point",
+        "standard_name": "cell_area",
+    }
+
     return dset
-
-
-def generate_time_stub(start_year=1981, nyears=5, calendar="noleap", frequency="MS"):
-    """Function to generate a dataset with a time coordinate
-
-    This function creates a "stub" dataset that can be used a starting
-    point for further test datasets.  It returns a cf-time index and
-    related FMS bounds and helper fields. Values for `nyears` are generated.
-
-    Parameters
-    ----------
-    start_year : int
-        Starting year for time coordinate, by default 1981
-    nyears : int
-        Number of years of monthly data to generate, by default 5
-    calendar : str
-        Cf-time recognized calendar, by default "noleap"
-    frequency : str
-        Frequency string compatible with xarray.cftime_range
-
-    Returns
-    -------
-    xarray.core.dataset.Dataset
-        Stub dataset with time coordinate
-    """
-
-    if frequency == "MS":
-        bounds = xr.cftime_range(
-            f"{start_year}-01-01",
-            freq=frequency,
-            periods=(nyears * 12) + 1,
-            calendar=calendar,
-        )
-        bounds = bounds.values
-
-    elif frequency == "D":
-        bounds = xr.cftime_range(
-            f"{start_year}-01-01",
-            freq=frequency,
-            periods=(nyears * 366) + 1,
-            calendar=calendar,
-        )
-        bounds = [
-            x
-            for x in bounds.values
-            if x <= cftime.datetime(start_year + nyears, 1, 1, calendar=calendar)
-        ]
-
-    else:
-        raise ValueError(f"Time frequency '{frequency}' is not currently supported.")
-
-    time_bnds = list(zip(bounds[0:-1], bounds[1::]))
-    bnds = np.array([1, 2])
-
-    time = [x[0] + (x[1] - x[0]) / 2 for x in time_bnds]
-    time = xr.DataArray(
-        time,
-        {"time": time},
-        attrs={
-            "long_name": "time",
-            "cartesian_axis": "T",
-            "calendar_type": calendar,
-            "bounds": "time_bnds",
-        },
-    )
-
-    time_bnds = xr.DataArray(time_bnds, {"time": time, "bnds": bnds})
-
-    average_T1 = xr.DataArray(time_bnds[:, 0].values, {"time": time})
-    average_T2 = xr.DataArray(time_bnds[:, 1].values, {"time": time})
-    average_DT = average_T2 - average_T1
-
-    return xr.Dataset(
-        {
-            "time": time,
-            "time_bnds": time_bnds,
-            "average_T1": average_T1,
-            "average_T2": average_T2,
-            "average_DT": average_DT,
-        }
-    )
